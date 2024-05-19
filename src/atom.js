dfa79ff26baea19d8a208a1ca539780184b5e412
shongCoder <soyeong163@gmail.com>
2024-05-19 18:36:21 +0900

    message write,  delete

--- src/atom.js
+++ src/atom.js
@@ -1,6 +1,23 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "messageLocal",
+  storage: localStorage,
+});
 
 export const messageState = atom({
-  key: "messageState",
+  key: "message",
   default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const nameState = atom({
+  key: "name",
+  default: "Name",
+});
+
+export const profileState = atom({
+  key: "profile",
+  default: "/img/profile_img.jpg",
 });
